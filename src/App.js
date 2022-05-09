c19878e3a8630ac3ffae24efaed9d4da0d0d3149
Fernando <fernando-perugachi@hotmail.com>
2022-05-09 13:58:13 -0500

    Add button repo to app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
         <InfoWheather/>
+        <a className='icon-git' href="https://github.com/MalanFernando/Weather" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-github"></i></a>
     </div>
   );
 }
